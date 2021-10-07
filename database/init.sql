0e8563de33d34dc599cbfe37da716023998cce16
bubbles linux <appenzeller.barbara@gmail.com>
2021-10-07 10:42:47 +0100

    Set up tables, column constraints, example inserts
    Co-authored-by: cerealenjoyer <cerealenjoyer@users.noreply.github.com>

--- database/init.sql
+++ database/init.sql
@@ -9,26 +9,41 @@ BEGIN;
 DROP TABLE IF EXISTS users, blog_posts CASCADE;
 
 CREATE TABLE reviews (
-  id SERIAL PRIMARY KEY,
-  place_id,
-  author_id,
-  heading,
-  review
+  id SERIAL PRIMARY KEY NOT NULL,
+  -- maybe id NOT NULL is not necessary? we left it in to be on the safe side for now
+  place_id INTEGER REFERENCES place(id),
+  author_id INTEGER REFERENCES user(id) ON DELETE CASCADE,
+  heading VARCHAR(50) NOT NULL,
+  review TEXT
 );
 
 CREATE TABLE place (
-  id SERIAL PRIMARY KEY,
-  place_name,
-  rating
+  id SERIAL PRIMARY KEY NOT NULL,
+  place_name VARCHAR(50) NOT NULL,
+  rating INTEGER
 );
 
 CREATE TABLE user (
-  id SERIAL PRIMARY KEY,
-  username
+  id SERIAL PRIMARY KEY NOT NULL,
+  username VARCHAR (30) NOT NULL
+  -- to handle username, we need a function: if a user doesn't enter a username --> pretend they entered 'Anonymous'. All non-username reviews are then attached to 'Anonymous' in the user table
 );
 
+INSERT INTO reviews (heading, review, place_id, author_id) VALUES
+  ('Just OK!', "The service was slow and some of the food had actual poison in it! Otherwise lovely place to eat.", 1, 1)
+;
 
--- Create tables and define their columns
+INSERT INTO place (place_name, rating) VALUES 
+  ('Picnic in the parc', 3), 
+  ('Bringing your own lunch', 5)
+;
+
+INSERT INTO user (user) VALUES 
+  ('Cyber_granny'),
+  ('Anonymous')
+;
+
+--  Create tables and define their columns
 
 -- CREATE TABLE users (
 --   id SERIAL PRIMARY KEY,
