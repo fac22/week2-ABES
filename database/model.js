838ec6102e989afa5c3eab5a75a68f7c897413fb
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-10-07 16:50:16 +0100

    wrote sql query and displayed results on home page
    Co-authored-by: Barbara <77367593+0bubbles0@users.noreply.github.com>

--- database/model.js
+++ database/model.js
@@ -3,7 +3,22 @@
 const db = require('./connection.js');
 
 function getReviews() {
-  return db.query('SELECT * FROM reviews').then((result) => result.rows);
+  // create an obj { restaurant-name, address, rating, author, review}
+  const queryStr = /*sql*/ `
+    SELECT
+      place.place_name,
+      place.postcode,
+      place.rating,
+      users.username,
+      reviews.review
+    FROM reviews
+    JOIN place
+      ON reviews.place_id = place.place_id
+    JOIN users
+      ON reviews.author_id = users.author_id
+  `;
+
+  return db.query(queryStr).then((result) => result.rows);
 }
 
 module.exports = { getReviews };
--- routes/home.js
+++ routes/home.js
@@ -5,28 +5,52 @@ const { getReviews } = require('../database/model.js');
 const layout = require('../layout');
 
 function get(request, response) {
-  getReviews().then((data) => console.log(data));
-  const html = /*html*/ `
-  <header>
-    <img src="" alt=""/>
-    <h1>FAC EATS</h1>
-  </header>
+  getReviews()
+    .then((data) =>
+      data.map(
+        (review) => /*html*/ `
+          <article>
+                <h2>${review.place_name}</h2>
+                <p>${review.postcode}</p>
+                <p>${review.rating.toString()}</p>
+                <p>${review.username}</p>
+                <p>${review.review}</p>
+            </article>
+          `
+      )
+    )
+    .then(
+      (reviewsHtml) => /*html*/ `
+          <header>
+            <img src="" alt=""/>
+            <h1>FAC EATS</h1>
+          </header>
 
-  <nav>
-    <a href="/review">Review</a>
-  </nav>
+          <nav>
+            <a href="/review">Review</a>
+          </nav>
 
-  <main>
-    <article>
-        <h2>Restaurant Name</h2>
-        <p>Address</p>
-        <p>Rating: </p>
-        <p>Author</p>
-        <p>Review</p>
-    </article>
-  </main>
-  `;
-  response.send(layout('Home', html));
+          <main>
+            ${reviewsHtml}
+          </main>
+          `
+    )
+    .then((homeHtml) => response.send(layout('Home', homeHtml)));
+  // const homeHtml = /*html*/ `
+  // <header>
+  //   <img src="" alt=""/>
+  //   <h1>FAC EATS</h1>
+  // </header>
+
+  // <nav>
+  //   <a href="/review">Review</a>
+  // </nav>
+
+  // <main>
+  //   ${reviewsHtml}
+  // </main>
+  // `;
+  // response.send(layout('Home', homeHtml));
 }
 
 module.exports = { get };
